b4231029b4fa5d0ce8d866b8ca2c14d1b6b2de65
Shannon McIntosh <smcintosh881@gmail.com>
2019-02-03 19:18:16 -0500

    countdown

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function potato() {
-    alert('potatoes');
+function countdown() {
+  let date = "March, 7, 2019";
+
+  let d1 = new Date(date);
+  let d2 = new Date();
+
+  let div = document.getElementById("count");
+  div.innerHTML +=  parseInt( ( d1.getTime() - d2.getTime() ) / ( 24 * 3600 * 1000 ) ) + ' days until snugs!';
 }
+
+countdown();
