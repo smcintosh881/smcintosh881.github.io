560e9247238574156e5360b4fea57395833b28e7
Shannon McIntosh <smcintosh881@gmail.com>
2019-04-26 16:46:39 -0400

    updates

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function countdown() {
-  let date = "March, 7, 2019";
+  let date = "May, 8, 2019";
 
   let d1 = new Date(date);
   let d2 = new Date();
 
   let div = document.getElementById("count");
-  div.innerHTML +=  parseInt( ( d1.getTime() - d2.getTime() ) / ( 24 * 3600 * 1000 ) ) + ' days until snugs!';
+  div.innerHTML =  parseInt( ( d1.getTime() - d2.getTime() ) / ( 24 * 3600 * 1000 ) ) + ' days until ' + temp();
 }
 
-countdown();
+function temp() {
+
+  const stuff = ["snugs", "cuddles", "new adventures", "🔥💦", "🏕️", "🏍️💨", "🍻", "👨‍💻👩‍💻", "💏"];
+  let i = Math.floor(Math.random() * stuff.length);
+  return stuff[i]
+}
+
+setInterval(countdown, 2000);
